fb4b623d0ee854a365530c9cd495ea7898d53773
Denis Magda <dmagda@gridgain.com>
2022-11-11 10:18:56 -0500

    changed login view's message

--- messenger/src/main/java/com/yugabyte/app/messenger/views/LoginView.java
+++ messenger/src/main/java/com/yugabyte/app/messenger/views/LoginView.java
@@ -21,7 +21,7 @@ public class LoginView extends LoginOverlay implements BeforeEnterObserver {
         LoginI18n i18n = LoginI18n.createDefault();
         i18n.setHeader(new LoginI18n.Header());
         i18n.getHeader().setTitle("geo-distributed-messenger");
-        i18n.getHeader().setDescription("Login using user/user or admin/admin");
+        i18n.getHeader().setDescription("Log in using test@gmail.com/password");
         i18n.setAdditionalInformation(null);
         setI18n(i18n);
 
