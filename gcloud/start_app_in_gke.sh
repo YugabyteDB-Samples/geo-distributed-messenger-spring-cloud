caaa9c418e311d19858b5d3d27724a6d8da8858c
Denis Magda <dmagda@gridgain.com>
2023-01-05 14:14:25 -0500

    completed messenger's gke instruction

--- gcloud/start_app_in_gke.sh
+++ gcloud/start_app_in_gke.sh
@@ -8,7 +8,7 @@ do
     esac
 done
 
-# Starting Config Server in GKE
+# Starting the Attachments microservice in GKE
 
 echo "Starting an Attachments pod in $region..."
 
@@ -20,3 +20,18 @@ echo "Starting an Attachments service in $region..."
 
 kubectl apply -f service-gke.yaml
 kubectl get service attachments-service
+
+# Starting the Messenger microservice in GKE
+
+echo "Starting a Messenger pod in $region..."
+
+cd ../messenger
+kubectl apply -f secret-gke.yaml
+
+cat deployment-gke-template.yaml | sed "s/_REGION/$region/g" | sed "s/_PROJECT_ID/$project_id/g" > deployment-gke.yaml
+kubectl apply -f deployment-gke.yaml
+
+echo "Starting a Messenger service in $region..."
+
+kubectl apply -f service-gke.yaml
+kubectl get service messenger-service
