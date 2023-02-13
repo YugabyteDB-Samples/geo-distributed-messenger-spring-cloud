6cef3c25a006e8daad30dc378d1f950fd54c48f1
Denis Magda <dmagda@gridgain.com>
2023-02-13 09:16:09 -0500

    removed unused imports

--- messenger/src/main/java/com/yugabyte/app/messenger/data/generator/DataGenerator.java
+++ messenger/src/main/java/com/yugabyte/app/messenger/data/generator/DataGenerator.java
@@ -7,10 +7,7 @@ import com.yugabyte.app.messenger.data.entity.Channel;
 import com.yugabyte.app.messenger.data.entity.Message;
 import com.yugabyte.app.messenger.data.entity.Profile;
 import com.yugabyte.app.messenger.data.entity.Workspace;
-import com.yugabyte.app.messenger.data.repository.ChannelRepository;
-import com.yugabyte.app.messenger.data.repository.MessageRepository;
 import com.yugabyte.app.messenger.data.repository.ProfileRepository;
-import com.yugabyte.app.messenger.data.repository.WorkspaceRepository;
 import com.yugabyte.app.messenger.data.service.DataGenerationService;
 
 import java.time.LocalDateTime;
@@ -26,7 +23,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringComponent
 public class DataGenerator {
